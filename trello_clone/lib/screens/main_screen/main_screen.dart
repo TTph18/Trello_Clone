1ca8cec2e8e6cf1fc9fb53bb1b3e4827fbb45d41
Châu Trần Lê Bảo <18520256@ms.uit.edu.vn>
2021-03-30 12:21:25 +0700

    add appbar and round button

--- trello_clone/lib/screens/main_screen/main_screen.dart
+++ trello_clone/lib/screens/main_screen/main_screen.dart
@@ -5,19 +5,47 @@ class MainScreen extends StatefulWidget {
   _MainScreenState createState() => _MainScreenState();
 }
 
+class MyAppBar extends AppBar with PreferredSizeWidget {
+  @override
+  get preferredSize => Size.fromHeight(50);
+  MyAppBar({Key key, Widget title}) : super(
+    key: key,
+    title: title,
+    automaticallyImplyLeading: false,
+    backgroundColor: Colors.white,
+  );
+}
+
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        child: SafeArea(
-          child: Column(
-            children: [
-              Center(child: Text("Hello MainScreen")),
-            ],
-          ),
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+        leading: new Container(
+          child: new Icon(Icons.menu),
         ),
+        title: Text("Board"),
+        actions: [
+          IconButton(icon: const Icon(Icons.search), onPressed: () {},)
+        ]
       ),
+      body: Column(
+        children: [
+          MyAppBar(
+            title: Text("'s workspace", style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.normal,
+              fontSize: 16,
+            ),),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.green,
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
