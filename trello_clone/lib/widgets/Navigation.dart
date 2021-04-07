d25ca088d85e3c54214d456d2e9fc3eb9a40d8c1
Châu Trần Lê Bảo <18520256@ms.uit.edu.vn>
2021-04-07 16:29:04 +0700

    Basic side navigation

--- trello_clone/lib/widgets/Navigation.dart
+++ trello_clone/lib/widgets/Navigation.dart
@@ -1,18 +1,128 @@
 import 'dart:ui';
+
 import 'package:flutter/material.dart';
 
-class Item extends StatelessWidget {
+class NavigationMain extends StatelessWidget {
+  List<String> namesTop = ["Bảng", "Trang chủ"];
+  List<String> namesBot = ["Thẻ của tôi", "Cài đặt", "Trợ giúp"];
+  List<String> grNames = ["Nhóm 1", "Nhóm 2"];
+  List<IconData> iconsTop = [Icons.dashboard, Icons.home];
+  List<IconData> iconsBot = [
+    Icons.credit_card,
+    Icons.settings,
+    Icons.info_outlined
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.fromLTRB(15, 10, 15, 5),
-      child: Row(
-        children: [
-          Icon(Icons.dashboard_sharp),
-          SizedBox(width: 20,),
-          Text("Bảng", style: TextStyle(fontSize: 16),),
-        ],
-      ),
+    return Column(
+      children: <Widget>[
+        //Board, Home
+        MediaQuery.removePadding(
+          context: context,
+          removeTop: true,
+          child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              itemCount: namesTop.length,
+              itemBuilder: (BuildContext context, int index) {
+                return GestureDetector(
+                  child: Container(
+                    padding: EdgeInsets.fromLTRB(15, 10, 15, 5),
+                    child: Row(
+                      children: [
+                        Icon(iconsTop[index]),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Text(
+                          namesTop[index],
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  ),
+                  onTap: () {},
+                );
+              }),
+        ),
+        Divider(
+          thickness: 2,
+        ),
+        Align(
+          alignment: Alignment.centerLeft,
+          child: Container(
+            padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+            child: Text("Nhóm",
+                textAlign: TextAlign.left,
+                style: TextStyle(fontWeight: FontWeight.bold)),
+          ),
+        ),
+        //Group name
+        MediaQuery.removePadding(
+          context: context,
+          removeTop: true,
+          child: GestureDetector(
+            child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: grNames.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    padding: EdgeInsets.fromLTRB(15, 10, 15, 5),
+                    child: Row(
+                      children: [
+                        Icon(Icons.group_outlined),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Text(
+                          grNames[index],
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+            onTap: () {},
+          ),
+        ),
+        Divider(
+          thickness: 2,
+        ),
+        //My card, Setting, Help
+        MediaQuery.removePadding(
+          context: context,
+          removeTop: true,
+          child: GestureDetector(
+            child: ListView.builder(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: namesBot.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    padding: EdgeInsets.fromLTRB(15, 10, 15, 5),
+                    child: Row(
+                      children: [
+                        Icon(iconsBot[index]),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        Text(
+                          namesBot[index],
+                          style: TextStyle(fontSize: 16),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+            onTap: () {},
+          ),
+        ),
+      ],
     );
   }
 }
@@ -28,28 +138,7 @@ class NavigationDrawer extends StatelessWidget {
               color: Colors.blue,
             ),
           ),
-          Item(),
-          Item(),
-          Divider(
-            thickness: 2,
-          ),
-          Text(
-              "Nhóm",
-              style: TextStyle(
-                  fontWeight: FontWeight.bold
-              )
-          ),
-          ListView.builder(
-              physics: ClampingScrollPhysics(),
-              shrinkWrap: true,
-              itemCount: 2,
-              itemBuilder: (BuildContext context, int index) {
-                return Item();
-              },
-            ),
-          Divider(
-            thickness: 2,
-          ),
+          NavigationMain(),
         ],
       ),
     );
