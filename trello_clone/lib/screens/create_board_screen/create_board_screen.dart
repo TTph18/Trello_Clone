8b0f125761adb2d8d8731cd9cbda57d0c66ff815
Cúc Phan Thị Hồng <18520260@ms.uit.edu.vn>
2021-04-23 11:50:59 +0700

    Delete create_board_form.dart

--- trello_clone/lib/screens/create_board_screen/create_board_screen.dart
+++ trello_clone/lib/screens/create_board_screen/create_board_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:trello_clone/widgets/create_board_screen/create_board_form.dart';
 import '../../route_path.dart';
 
 class CreateBoardScreen extends StatefulWidget {
@@ -8,6 +7,12 @@ class CreateBoardScreen extends StatefulWidget {
 }
 
 class CreateBoardScreenState extends State<CreateBoardScreen> {
+
+  final _createBoardFormKey = GlobalKey<FormState>();
+  var _nameTxtCtrl = TextEditingController();
+  var _groupTxtCtrl = TextEditingController();
+  var _permissionTxtCtrl = TextEditingController();
+  String? _groupCtrl = "Ngáo";
 
   @override
   Widget build(BuildContext context) {
@@ -26,14 +31,89 @@ class CreateBoardScreenState extends State<CreateBoardScreen> {
           actions: [
         IconButton(
           icon: const Icon(Icons.check),
-          onPressed: () {},
+          onPressed: () {
+            if (_createBoardFormKey.currentState!.validate())
+              {
+                Navigator.of(context).pushNamed(MAIN_SCREEN);
+              }
+          },
         ),
       ]),
       body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          CreateBoardForm()
+        children: [Form(
+          key: _createBoardFormKey,
+          child: Padding(
+            padding: const EdgeInsets.only(left: 25.0, right: 25.0, top: 25.0),
+            child: Column(
+              children: <Widget>[
+                TextFormField(
+                  controller: _nameTxtCtrl,
+                  decoration: InputDecoration(
+                    labelStyle: TextStyle(fontSize: 18.0, color: Colors.green),
+                    labelText: "Tên bảng",
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    contentPadding: EdgeInsets.only(bottom: 0.0),
+                    focusedBorder:UnderlineInputBorder(
+                      borderSide: const BorderSide(color: Colors.green),
+                    ),
+                  ),
+                  textAlign: TextAlign.left,
+                  style: TextStyle(fontSize: 22.0),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Tên bảng không được để trống';
+                    }
+                    return null;
+                    },
+                ),
+                TextFormField(
+                  controller: _groupTxtCtrl,
+                  decoration: InputDecoration(
+                      hintText: "Nhóm"
+                  ),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Nhóm không được để trống';
+                    }
+                    return null;
+                    },
+                ),
+                TextFormField(
+                  controller: _permissionTxtCtrl,
+                  decoration: InputDecoration(
+                      hintText: "Quyền xem"
+                  ),
+                  // The validator receives the text that the user has entered.
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Quyền xem không được để trống';
+                    }
+                    return null;
+                    },
+                ),
+                DropdownButtonFormField<String>(
+                  value: _groupCtrl,
+                  items: ["Ngáo", "Shop Ngáo", "Ngáo Ngơ"].map((group) => DropdownMenuItem(
+                    child: Text(group),
+                    value: group,
+                  )).toList(),
+                  decoration: InputDecoration(
+                    labelStyle: TextStyle(fontSize: 18.0, color: Colors.green),
+                    labelText: "Nhóm",
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                  ),
+                  onChanged: (value) {
+                    setState(() {
+                      _groupCtrl = value;
+                    });
+                    },
+                ),
+              ],
+            ),
+          ),
+        ),
         ],
       ),
     );
