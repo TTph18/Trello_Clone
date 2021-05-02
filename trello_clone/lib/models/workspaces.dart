533be032e06ab9f9b7c4c6b778f3d9976844b02f
TranThiHaPhuong <59615434+TTph18@users.noreply.github.com>
2021-05-02 15:29:08 +0700

    Add validate and data service (unfinished)

--- trello_clone/lib/models/workspaces.dart
+++ trello_clone/lib/models/workspaces.dart
@@ -3,15 +3,13 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class Workspaces {
 
   final List<String> boardList;
-  final List<String> userList;
   final String workspaceName;
 
-  Workspaces({required this.boardList, required this.userList, required this.workspaceName});
+  Workspaces({required this.boardList, required this.workspaceName});
 
   factory Workspaces.fromDocument(DocumentSnapshot document) {
     return Workspaces(
       boardList: document['boardList'],
-      userList: document['userList'],
       workspaceName: document['workspaceName']
     );
   }
--- trello_clone/lib/services/database.dart
+++ trello_clone/lib/services/database.dart
@@ -1,6 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:provider/provider.dart';
+import 'package:trello_clone/models/user.dart';
 
 class DatabaseService {
+  final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+  Stream<String?> get onAuthStateChanged => _firebaseAuth.authStateChanges().map(
+        (User? user) => user?.uid,
+  );
+
+  static Future<Users> getUserData(context) async {
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+    var snapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .where('userID', isEqualTo: uid)
+        .get();
+    return Users.fromDocument(snapshot.docs.first);
+  }
 
 
 }
