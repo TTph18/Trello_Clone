00fcefea3a0d19ee13c4d379e4fb90a51a1b3e93
Cúc Phan Thị Hồng <18520260@ms.uit.edu.vn>
2021-07-05 01:21:35 +0700

    Fix create_card_screen.dart (unfinished)

--- trello_clone/lib/screens/create_card_screen/create_card_screen.dart
+++ trello_clone/lib/screens/create_card_screen/create_card_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:trello_clone/icons/app_icons.dart';
+import 'package:trello_clone/models/user.dart';
 import '../../route_path.dart';
 
 class CreateCardScreen extends StatefulWidget {
@@ -7,9 +9,32 @@ class CreateCardScreen extends StatefulWidget {
   CreateCardScreenState createState() => CreateCardScreenState();
 }
 
+class BoardItem {
+  String name;
+  String type;
+
+  BoardItem({required this.name, required this.type});
+}
+
 class CreateCardScreenState extends State<CreateCardScreen> {
   final formKey = GlobalKey<FormState>();
-  String? selectedBoard = "";
+  String selectedBoard = "";
+  List<String> groupName = ["Tên nhóm 1", "Tên nhóm 2", "Tên nhóm 3"];
+  List<BoardItem> boardItems = [
+    BoardItem(name: "Tên nhóm 1", type: "sep"),
+    BoardItem(name: "Tên bảng 1 nhóm 1", type: "data"),
+    BoardItem(name: "Tên bảng 2 nhóm 1", type: "data"),
+    BoardItem(name: "Tên nhóm 2", type: "sep"),
+    BoardItem(name: "Tên bảng 1 nhóm 2", type: "data"),
+    BoardItem(name: "Tên bảng 2 nhóm 2", type: "data"),
+    BoardItem(name: "Tên bảng 3 nhóm 2", type: "data"),
+    BoardItem(name: "Tên bảng 4 nhóm 2", type: "data"),
+    BoardItem(name: "Tên nhóm 3", type: "sep"),
+    BoardItem(name: "Tên bảng 1 nhóm 3", type: "data"),
+    BoardItem(name: "Tên bảng 2 nhóm 3", type: "data"),
+    BoardItem(name: "Tên bảng 3 nhóm 3", type: "data"),
+  ];
+
   List<String> boardList = ["Tên bảng 1", "Tên bảng 2", "Tên bảng 3"];
   String? selectedList = "";
   List<String> listList = [
@@ -17,7 +42,42 @@ class CreateCardScreenState extends State<CreateCardScreen> {
     "Tên danh sách 2",
     "Tên danh sách 3"
   ];
-  List<String> nameList = ["Châu", "Cúc", "Phương"];
+  List<Users> users = [
+    Users(
+      userID: "12345",
+      userName: "name1",
+      profileName: "Name 1",
+      email: '123456@gmail.com',
+      avatar: 'assets/images/BlueBG.png',
+      workspaceList: [],
+    ),
+    Users(
+      userID: "12345",
+      userName: "name2",
+      profileName: "Name 2",
+      email: '123456@gmail.com',
+      avatar: 'assets/images/BlueBG.png',
+      workspaceList: [],
+    ),
+    Users(
+      userID: "12345",
+      userName: "name3",
+      profileName: "Name 3",
+      email: '123456@gmail.com',
+      avatar: 'assets/images/BlueBG.png',
+      workspaceList: [],
+    ),
+    Users(
+      userID: "12345",
+      userName: "Test4",
+      profileName: "Cun cun cute",
+      email: '123456@gmail.com',
+      avatar: 'assets/images/BlueBG.png',
+      workspaceList: [],
+    ),
+  ];
+
+  List<Users> pickedUsers = [];
 
   var cardNameTxtCtrl = TextEditingController();
   var descriptionTxtCtrl = TextEditingController();
@@ -46,47 +106,106 @@ class CreateCardScreenState extends State<CreateCardScreen> {
         key: formKey,
         child: Column(
           children: <Widget>[
-            Padding(
-              padding: const EdgeInsets.only(left: 20, right: 20, top: 20),
-              child: DropdownButtonFormField<String>(
-                icon: Icon(Icons.keyboard_arrow_down),
-                hint: Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    "Chọn bảng",
-                    style: TextStyle(fontSize: 20.0),
+            SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              width: MediaQuery.of(context).size.width * 0.9,
+              child: ButtonTheme(
+                child: DropdownButtonFormField<String>(
+                  icon: Icon(Icons.keyboard_arrow_down),
+                  hint: Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      "Chọn bảng",
+                      style: TextStyle(fontSize: 20.0),
+                    ),
                   ),
-                ),
-                decoration: InputDecoration(
-                  labelStyle: TextStyle(fontSize: 18.0, height: 0.9),
-                  labelText: "Bảng",
-                  floatingLabelBehavior: FloatingLabelBehavior.always,
-                  contentPadding: EdgeInsets.only(bottom: 0),
-                ),
-                onChanged: (value) {
-                  setState(() {
-                    selectedBoard = value;
-                  });
-                },
-                selectedItemBuilder: (BuildContext context) {
-                  return boardList.map<Widget>((String item) {
-                    return Text(
-                      item,
-                      style: TextStyle(
-                        fontSize: 20.0,
+                  decoration: InputDecoration(
+                    labelStyle: TextStyle(fontSize: 18.0, height: 0.9),
+                    labelText: "Bảng",
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                    contentPadding: EdgeInsets.only(bottom: 0),
+                  ),
+                  onChanged: (value) {
+                    if (value != null)
+                      print("VALUE: " + value);
+                    else
+                      print("NOT VALUE");
+                    if (boardItems[boardItems
+                        .indexWhere((element) => element.name == value)]
+                        .type ==
+                        "sep") {
+                      return;
+                    }
+                    setState(() {
+                      if (value != null) selectedBoard = value;
+                    });
+                  },
+                  selectedItemBuilder: (BuildContext context) {
+                    return boardItems.map((BoardItem item) {
+                      return Text(
+                        selectedBoard,
+                        style: TextStyle(
+                          fontSize: 20.0,
+                        ),
+                      );
+                    }).toList();
+                  },
+                  items: boardItems.map((BoardItem item) {
+                    return DropdownMenuItem(
+                      value: item.name,
+                      onTap: item.type == "data" ? () {} : null,
+                      child: item.type == "data"
+                          ? Container(
+                        padding: EdgeInsets.fromLTRB(15, 7, 15, 7),
+                        child: Row(
+                          children: [
+                            Container(
+                              padding: EdgeInsets.fromLTRB(0, 0, 15, 0),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(3.0),
+                                child: Image(
+                                  image: AssetImage(
+                                      "assets/images/BlueBG.png"),
+                                  width: 50,
+                                  height: 50,
+                                ),
+                              ),
+                            ),
+                            Text(
+                              item.name,
+                              style: TextStyle(fontSize: 20),
+                            ),
+                          ],
+                        ),
+                      )
+                          : Container(
+                        height: 50,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          border: Border(
+                            top: BorderSide(
+                                width: 1.0, color: Colors.black),
+                            bottom: BorderSide(
+                                width: 1.0, color: Colors.black),
+                          ),
+                        ),
+                        child: Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            item.name,
+                            style: TextStyle(
+                              color: Colors.black,
+                              fontWeight: FontWeight.normal,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ),
                       ),
                     );
-                  }).toList();
-                },
-                items: boardList.map((String item) {
-                  return DropdownMenuItem<String>(
-                      value: item,
-                      child: Row(
-                        children: [
-                          Text(item, style: TextStyle(fontSize: 20.0)),
-                        ],
-                      ));
-                }).toList(),
+                  }).toList(),
+                ),
               ),
             ),
             Padding(
@@ -109,10 +228,10 @@ class CreateCardScreenState extends State<CreateCardScreen> {
                 onChanged: selectedBoard == ""
                     ? null
                     : (value) {
-                        setState(() {
-                          selectedList = value;
-                        });
-                      },
+                  setState(() {
+                    selectedList = value;
+                  });
+                },
                 selectedItemBuilder: (BuildContext context) {
                   return listList.map<Widget>((String item) {
                     return Text(
@@ -188,57 +307,83 @@ class CreateCardScreenState extends State<CreateCardScreen> {
                       ),
                       Padding(
                         padding: const EdgeInsets.only(top: 15),
-                        child: Table(
-                          columnWidths: const <int, TableColumnWidth>{
-                            0: IntrinsicColumnWidth(),
-                            1: FlexColumnWidth(),
-                          },
+                        child: Column(
                           children: [
+                            ///Members here
                             selectedBoard == ""
-                                ? TableRow(
-                                    children: [
-                                      SizedBox(),
-                                      SizedBox(),
-                                    ],
-                                  )
-                                : TableRow(
-                                    children: [
-                                      IconButton(
-                                        icon: Icon(MyFlutterApp.person_outline),
-                                        alignment: Alignment.centerLeft,
-                                        onPressed: () {},
-                                      ),
-                                      Container(
-                                        alignment: Alignment.centerLeft,
-                                        child: CircleAvatar(
-                                            radius: 25,
-                                            backgroundColor: Colors.green,
-                                            child: PopupMenuButton(
-                                              itemBuilder: (context) =>
-                                                  List.generate(
-                                                nameList.length,
-                                                (index) => PopupMenuItem(
-                                                  value: nameList[index],
-                                                  child: ListTile(
-                                                    leading: CircleAvatar(
-                                                      child: Text(
-                                                          '${nameList[index][0]}'),
-                                                    ),
-                                                    title: Text(
-                                                        '${nameList[index]}'),
+                                ? SizedBox()
+                                : Row(
+                              children: [
+                                IconButton(
+                                  icon: Icon(MyFlutterApp.person_outline),
+                                  alignment: Alignment.centerLeft,
+                                  onPressed: () {},
+                                ),
+                                Container(
+                                  alignment: Alignment.centerLeft,
+                                  child: CircleAvatar(
+                                      radius: 25,
+                                      backgroundColor: Colors.green,
+                                      child: PopupMenuButton<Users>(
+                                        itemBuilder: (context) =>
+                                            List.generate(
+                                              users.length,
+                                                  (index) => PopupMenuItem<Users>(
+                                                value: users[index],
+                                                child: ListTile(
+                                                  leading: CircleAvatar(
+                                                    backgroundImage:
+                                                    AssetImage(
+                                                        users[index]
+                                                            .avatar),
                                                   ),
+                                                  title: Text(
+                                                      '${users[index].userName}'),
                                                 ),
                                               ),
-                                              icon: Icon(
-                                                Icons.add,
-                                                color: Colors.white,
-                                                size: 18,
-                                              ),
-                                            )),
-                                      ),
-                                    ],
-                                  ),
-                            TableRow(
+                                            ),
+                                        onSelected: (value) {
+                                          setState(() {
+                                            pickedUsers.add(value);
+                                            print("Length = " +
+                                                pickedUsers.length
+                                                    .toString());
+                                          });
+                                        },
+                                        icon: Icon(
+                                          Icons.add,
+                                          color: Colors.white,
+                                          size: 18,
+                                        ),
+                                      )),
+                                ),
+                                pickedUsers.length < 1
+                                    ? SizedBox()
+                                    : Container(
+                                  width: MediaQuery.of(context)
+                                      .size
+                                      .width *
+                                      0.5,
+                                  child: ListView.builder(
+                                      shrinkWrap: true,
+                                      scrollDirection:
+                                      Axis.horizontal,
+                                      itemCount: pickedUsers.length,
+                                      itemBuilder:
+                                          (context, index) {
+                                        return CircleAvatar(
+                                          backgroundImage:
+                                          AssetImage(
+                                              pickedUsers[index]
+                                                  .avatar),
+                                        );
+                                      }),
+                                ),
+                              ],
+                            ),
+
+                            ///DateStart
+                            Row(
                               children: [
                                 IconButton(
                                   icon: Icon(MyFlutterApp.clock),
@@ -254,15 +399,19 @@ class CreateCardScreenState extends State<CreateCardScreen> {
                                   style: ButtonStyle(
                                     alignment: Alignment.bottomLeft,
                                     overlayColor:
-                                        MaterialStateProperty.all<Color>(
-                                            Colors.white),
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.white),
                                   ),
                                 ),
                               ],
                             ),
-                            TableRow(
+
+                            ///DateEnd
+                            Row(
                               children: [
-                                SizedBox(),
+                                SizedBox(
+                                  width: 48,
+                                ),
                                 TextButton(
                                   onPressed: () {},
                                   child: Text("Ngày hết hạn...",
@@ -272,8 +421,8 @@ class CreateCardScreenState extends State<CreateCardScreen> {
                                   style: ButtonStyle(
                                     alignment: Alignment.centerLeft,
                                     overlayColor:
-                                        MaterialStateProperty.all<Color>(
-                                            Colors.white),
+                                    MaterialStateProperty.all<Color>(
+                                        Colors.white),
                                   ),
                                 ),
                               ],
